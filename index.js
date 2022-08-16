51dcf59698fe118a283934251e7e3e68c0f0d425
catherineotondi <catherineotondi@gmail.com>
2022-08-16 17:31:12 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    prompt('What have you done');
+    const input = document.getElementById("button");
+    input.addEventListener('click', addingEventListener);
 }
+
